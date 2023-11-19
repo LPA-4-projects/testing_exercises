939f2bd61690e3810d31c9f062ce70f75fefe75f
Daria Veselkova <daria.veselkova@yandex.ru>
2023-11-19 18:25:13 +0300

    feat: add tests for the first finction

--- tests/level_1/test_one_gender.py
+++ tests/level_1/test_one_gender.py
@@ -1,5 +1,44 @@
+import pytest
+
 from functions.level_1.one_gender import genderalize
 
 
-def test_genderalize():
-    pass
+def test__genderalize__return_male_verb_if_male(faker):
+    verb_male = faker.pystr()
+    verb_female = faker.pystr()
+    gender = 'male'
+
+    answer = genderalize(
+        verb_male=verb_male,
+        verb_female=verb_female,
+        gender=gender,
+    )
+
+    assert answer == verb_male
+
+
+def test__genderalize__return_female_verb_if_female(faker):
+    verb_male = faker.pystr()
+    verb_female = faker.pystr()
+    gender = 'female'
+
+    answer = genderalize(
+        verb_male=verb_male,
+        verb_female=verb_female,
+        gender=gender,
+    )
+
+    assert answer == verb_female
+
+
+def test__genderalize__raise_error_if_wrong_gender_word(faker):
+    verb_male = faker.pystr()
+    verb_female = faker.pystr()
+    gender = 'alien'
+
+    with pytest.raises(ValueError):
+        genderalize(
+            verb_male=verb_male,
+            verb_female=verb_female,
+            gender=gender,
+        )
