9d1aa4fdc708a3fa3efc5ee5bf0a67a9ac9052d7
Daria Veselkova <daria.veselkova@yandex.ru>
2023-11-19 20:00:08 +0300

    feat: tests for the second function

--- tests/level_1/test_two_date_parser.py
+++ tests/level_1/test_two_date_parser.py
@@ -1,5 +1,44 @@
+import datetime
+import pytest
+
 from functions.level_1.two_date_parser import compose_datetime_from
 
 
-def test_compose_datetime_from():
-    pass
+@pytest.fixture
+def make_time_str(faker) -> str:
+    hours, minutes, _ = faker.time().split(':')
+    return f'{hours}:{minutes}'
+
+
+def test__compose_datetime_from__return_correct_datetime(make_time_str):
+    now = datetime.date.today()
+
+    time_str = make_time_str
+    hours, minutes = time_str.split(':')
+    time = datetime.time(hour=int(hours), minute=int(minutes))
+
+    answer = compose_datetime_from(date_str='now', time_str=time_str)
+
+    assert type(answer) == datetime.datetime
+    assert now.day == answer.day
+    assert now.month == answer.month
+    assert now.year == answer.year
+    assert time.hour == answer.hour
+    assert time.minute == answer.minute
+
+
+def test__compose_datetime_from__return_correct_datetime_for_tomorrow_date_str(make_time_str):
+    tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+
+    time_str = make_time_str
+    hours, minutes = time_str.split(':')
+    time = datetime.time(hour=int(hours), minute=int(minutes))
+
+    answer = compose_datetime_from(date_str='tomorrow', time_str=time_str)
+
+    assert type(answer) == datetime.datetime
+    assert tomorrow.day == answer.day
+    assert tomorrow.month == answer.month
+    assert tomorrow.year == answer.year
+    assert time.hour == answer.hour
+    assert time.minute == answer.minute
